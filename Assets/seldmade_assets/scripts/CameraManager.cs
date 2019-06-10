7858dd2488a57d1610e7db1e70c2f2cf851297a0
Remco Liqui lung <36478397+BoMamaG@users.noreply.github.com>
2019-06-10 20:06:20 +0200

    g

---
+++ Assets/seldmade_assets/scripts/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    public GameObject[] cameras;
+    public GameObject startCamera;
+	// Use this for initialization
+	void Start () {
+        cameras = GameObject.FindGameObjectsWithTag("Camera");
+
+        for(int i = 0; i < cameras.Length; i++)
+        {
+            // It turns off all cameras in the scene
+            cameras[i].SetActive(false);
+        }
+        startCamera.SetActive(true);
+	}
+
+    public void DeactivateAllCameras()
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            // It turns off all cameras in the scene
+            cameras[i].SetActive(false);
+        }
+        startCamera.SetActive(true);
+    }
+}
+
+
